e9827e635476e579bfee7fb5268d9099a887b6a6
appadc <gutocastroneto@hotmail.com>
2026-09-23 01:32:18 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
    sincroniza os aparelhos.
    ============================================================= */
 
-const VERSION = 'v8.10';
+const VERSION = 'v8.11';
 const CACHE   = 'eurotrip26-' + VERSION;
 
 const ASSETS = [
